32bd20416143b645405ce86d9781dbe17a0cc4e5
Stéphane Girault <stephane.girault@gmail.com>
2011-08-26 16:10:14 +0200

    bundle imagine pour redimensionnement des images

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
 
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
 
             new Lapaperie\MainBundle\LapaperieMainBundle(),
             new Lapaperie\AgendaBundle\LapaperieAgendaBundle(),
