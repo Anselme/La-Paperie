79d53a89247fe0e1d772c67d44ab404ed75f947a
Anselme <stephane@letsroot.net>
2011-09-12 16:16:59 +0200

    fckeditor focntionnel

---
+++ src/Lapaperie/MainBundle/Resources/public/ckeditor/config.js
@@ -0,0 +1,15 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	 config.language = 'fr';
+	 config.uiColor = '#E32119';
+     config.toolbar_Bibi = 
+         [
+         [ 'Source', '-', 'Bold', 'Italic', 'FontSize', '-','NumberedList','BulletedList','-', 'Link','Unlink' ]
+         ];
+};
