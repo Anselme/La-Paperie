5de9d3150ccec31157ce36718e38d702752d9aca
Anselme <stephane@letsroot.net>
2011-10-19 17:51:12 +0200

    affichage video ordre aleatoire

--- src/Lapaperie/VideoBundle/Controller/VideoController.php
+++ src/Lapaperie/VideoBundle/Controller/VideoController.php
@@ -25,7 +25,10 @@ class VideoController extends Controller
     public function indexAction()
     {
         $repository = $this->getDoctrine()->getRepository('LapaperieVideoBundle:Video');
-        $videos = $repository->findAllOrderByRandom();
+        $videos = $repository->findAll();
+
+        //impossible de faire un ORDRER BY RAND avec Doctrine 2 ? on secoue donc le résultat
+        shuffle($videos);
 
         return $this->render('LapaperieVideoBundle:Default:index.html.twig', array('videos' => $videos));
     }
